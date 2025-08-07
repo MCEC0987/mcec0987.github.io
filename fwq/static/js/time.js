f3996c7f4c617028fdb211c796fd8742b2a05b41
MCEC0987 <150608924+MCEC0987@users.noreply.github.com>
2025-08-07 16:42:16 +0800

    更新 time.js

--- fwq/static/js/time.js
+++ fwq/static/js/time.js
@@ -12,7 +12,7 @@ function time() {
     var todayHour = today.getHours()
     var todayMinute = today.getMinutes()
     var todaySecond = today.getSeconds()
-    var t1 = Date.UTC(2023, 0, 0, 00, 00, 00)
+    var t1 = Date.UTC(2025, 7, 18, 00, 00, 00)
     var t2 = Date.UTC(todayYear, todayMonth, todayDate, todayHour, todayMinute, todaySecond)
     var diff = t2 - t1
     var diffYears = Math.floor(diff / years)
